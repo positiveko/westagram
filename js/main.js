1bfd161d73d285641a0397579341d836b83aed3f
positiveko <edie_ko@naver.com>
2020-10-24 20:59:40 +0900

    Edit JavaScript

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
-const searchInput = document.querySelector('.searchInput'),
+//8시 30분까지 인풋바 완성
+// 그 후 댓글 기능 활성화
+// 그 후 로그인해서 페이지 연결까지 시킬 것
+
+
+const search = document.querySelector('.search'),
+    searchInput = document.querySelector('.searchInput'),
     xbtn = document.querySelector(".xbtn"),
     magnifier = document.querySelector('.magnifier'),
-    searchText = document.querySelector('searchText');
+    searchText = document.querySelector('.searchText');
 
 
 function searchActive() {
-    xbtn.style.display = "block";
+    magnifier.classList.toggle('active');
+    searchInput.classList.toggle('active');
+    xbtn.classList.toggle('active');
+    searchText.classList.toggle('active');
 }
-
-function deleteInput() {
-    xbtn.style.display = "none";
-    searchText.style.display = "block";
-
-}
-
-searchInput.addEventListener('auxclick', searchActive());
