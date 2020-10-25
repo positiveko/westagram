317f0a80694591f5c9566bda57d2271c5ea45441
positiveko <edie_ko@naver.com>
2020-10-25 22:39:30 +0900

    Add comment function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
-// 그 후 댓글 기능 활성화 (로컬에 저장해서 새로고침해도 나오도록)
-// 그 후 로그인해서 페이지 연결까지 시킬 것(패스워드 틀릴 경우 체크)
-
-//# 메인 페이지
-// 1. 댓글 input 창에 엔터치거나 "게시" 누르면 댓글 추가되도록
-// - createElement로 요소 생성해서, input에 입력한 값이 
-// 추가 되어야합니다.
-
 const search = document.querySelector('.search'),
     searchInput = document.querySelector('.searchInput'),
     xbtn = document.querySelector(".xbtn"),
@@ -14,49 +6,45 @@ const search = document.querySelector('.search'),
 
 const commentForm = document.querySelector('.commentForm'),
     commentInput = document.querySelector('.commentInput'),
-    addBtn = document.querySelector('.addBtn');
-    commentList = document.querySelector('comment');
+    addBtn = document.querySelector('.addBtn'),
+    commentList = document.querySelector('.comment'),
+    commentArea = document.querySelector('.commentArea');
 
 // 만약 search를 클릭하면, 돋보기 좌측 보내기, 검색 텍스트 없애기,
 // 엑스버튼 뜨게하기, 인풋바 활성화하기,
 // 엑스버튼 클릭하면 돋보기 센터, 인풋바 없애기, 
 
 function searchActive() {
-    searchText.style.display = 'none';
     xbtn.style.display = 'block';
-    searchInput.display = 'none';
+    searchInput.classList.toggle('active');
     magnifier.classList.toggle('active');
-};
-
-function searchInactive() {
-    searchText.style.display = 'block';
-    xbtn.style.display='none';
-    searchInput.display = 'block';
-    magnifier.classList.toggle('active');
-
+    search.style.justifyContent = 'space-between';
 }
 
+function searchInactive() {
+    xbtn.style.display = 'none';
+    searchInput.classList.toggle('active');
+    magnifier.classList.toggle('active');
+    search.style.justifyContent = 'center';
+}
 
-
-
-
-
+// 받은 comment 추가하기
 function makeCmt(cmt) {
+    if (cmt.trim() === '') {
+        return;
+    }
     let codeblock = `<div class="commentSet">
     <span class="userId">edie_ko</span>
     <span class="userComment">${cmt}</span>
     </div>
     <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png" alt="smallHeart" class="smallHeart">`
-    console.log(codeblock);
-    let cmtSet = commentList.createElement('div')
-    cmtSet.innerText = codeblock;
-
-
-
-
+    let cmtSet = document.createElement('div');
+    cmtSet.className = 'comment';
+    cmtSet.innerHTML = codeblock;
+    commentArea.appendChild(cmtSet);
 }
 
-// 코멘트 폼에서 입력하면 실행
+// 코멘트 폼으로 comment 받기
 function submitCmt(event) {
     event.preventDefault();
     const cmt = commentInput.value;
@@ -66,7 +54,10 @@ function submitCmt(event) {
 
 
 search.addEventListener('click', searchActive);
-search.addEventListener('blur', searchInactive);
+searchInput.addEventListener('blur', searchInactive);
 xbtn.addEventListener('click', searchInactive);
+
+
 commentForm.addEventListener('submit', submitCmt);
 addBtn.addEventListener('click', submitCmt);
+
