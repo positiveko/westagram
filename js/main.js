143ca67687e85fc9e6a371fed6489642e27eb5e9
positiveko <edie_ko@naver.com>
2020-10-25 20:54:15 +0900

    Add Media Query

--- js/main.js
+++ js/main.js
@@ -12,6 +12,11 @@ const search = document.querySelector('.search'),
     magnifier = document.querySelector('.magnifier'),
     searchText = document.querySelector('.searchText');
 
+const commentForm = document.querySelector('.commentForm'),
+    commentInput = document.querySelector('.commentInput'),
+    addBtn = document.querySelector('.addBtn');
+    commentList = document.querySelector('comment');
+
 // 만약 search를 클릭하면, 돋보기 좌측 보내기, 검색 텍스트 없애기,
 // 엑스버튼 뜨게하기, 인풋바 활성화하기,
 // 엑스버튼 클릭하면 돋보기 센터, 인풋바 없애기, 
@@ -21,7 +26,47 @@ function searchActive() {
     xbtn.style.display = 'block';
     searchInput.display = 'none';
     magnifier.classList.toggle('active');
-    this.classList.add('inactive');
 };
 
+function searchInactive() {
+    searchText.style.display = 'block';
+    xbtn.style.display='none';
+    searchInput.display = 'block';
+    magnifier.classList.toggle('active');
+
+}
+
+
+
+
+
+
+function makeCmt(cmt) {
+    let codeblock = `<div class="commentSet">
+    <span class="userId">edie_ko</span>
+    <span class="userComment">${cmt}</span>
+    </div>
+    <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png" alt="smallHeart" class="smallHeart">`
+    console.log(codeblock);
+    let cmtSet = commentList.createElement('div')
+    cmtSet.innerText = codeblock;
+
+
+
+
+}
+
+// 코멘트 폼에서 입력하면 실행
+function submitCmt(event) {
+    event.preventDefault();
+    const cmt = commentInput.value;
+    makeCmt(cmt);
+    commentInput.value='';
+}
+
+
 search.addEventListener('click', searchActive);
+search.addEventListener('blur', searchInactive);
+xbtn.addEventListener('click', searchInactive);
+commentForm.addEventListener('submit', submitCmt);
+addBtn.addEventListener('click', submitCmt);
